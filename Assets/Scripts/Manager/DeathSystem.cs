ad0819fb9de8c0d03d8c6b75e2c503fe89c0b9ea
frevil <850675694@qq.com>
2026-03-05 21:30:12 +0800

    refactor: split UnitManager loop into 4 systems

---
+++ Assets/Scripts/Manager/DeathSystem.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Core;
+using Scripts.Core;
+using UnityEngine;
+
+namespace Manager
+{
+    public static class DeathSystem
+    {
+        public static void HandleDeath(List<UnitRuntimeData> units)
+        {
+            for (int i = 0; i < units.Count; i++)
+            {
+                var unit = units[i];
+                if (!unit.alive) continue;
+                if (unit.hp > 0) continue;
+
+                unit.alive = false;
+                units[i] = unit;
+
+                Debug.Log($"_{unit.name}_噶了");
+                if (unit.faction == Faction.Enemy)
+                {
+                    LevelSystem.GotExp(unit.killExp);
+                }
+            }
+        }
+    }
+}
